bbc165db5b4cc04841bf0c1d3ec4ceeca24b85b3
ejcanoy <euan.james.canoy@gmail.com>
2023-10-12 09:40:06 -0700

    added first draft of navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-        
+      <div>home</div>
       <Footer />
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
-
+import { ShoppingCart } from "react-feather"
 function Navbar() {
     return (
         <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                        <Link to="/new">New</Link>
-                        <Link to="/about">About</Link>
-                        <Link to="/shop">Shop</Link>
+            <nav className="flex w-full items-center bg-[#cdaf8c]">
+                <div className="pl-5">
+                    <Link to="/"><img src="/fnslogo.webp" alt="" /></Link>
+                </div>
+                <ul className="w-full flex justify-center">
+                    <li className="space-x-5">
+                        <Link className="w-[50px]" to="/">HOME</Link>
+                        <Link className="w-[50px]" to="/new">NEW!</Link>
+                        <Link className="w-[50px]" to="/about">ABOUT</Link>
+                        <Link className="w-[50px]" to="/shop">SHOP</Link>
                     </li>
                 </ul>
+                <div className="min-w-[290px] min-h-[130px] flex justify-center items-center pr-5">
+                    <button>
+                        <ShoppingCart />
+                    </button>
+                </div>
             </nav>
         </>
     )
