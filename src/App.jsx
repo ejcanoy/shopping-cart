c85936458a798b7893a244220e70c89adddde137
ejcanoy <euan.james.canoy@gmail.com>
2023-10-17 11:50:55 -0700

    modified carousel position

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Carousel showArrows={true} showThumbs={false} showStatus={false} autoPlay={false} infiniteLoop={false}>
         <div>
           <img src="/cover1152x540.webp" />
-          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 w-11/12 p-2">
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 xl:-translate-y-[400%] 2xl:-translate-y-[800%]  w-11/12 p-2">
             <button className="bg-[#cdaf8c] ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] rounded-lg h-11 font-semibold" onClick={() => { console.log("Here") }}>
               <p className="w-[200px]">Explore Fall Collection</p>
             </button>
@@ -19,7 +19,7 @@ const App = () => {
         </div>
         <div>
           <img src="/storySZN960x450.webp" />
-          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 w-11/12 p-2">
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 xl:-translate-y-[400%] 2xl:-translate-y-[800%] w-11/12 p-2">
             <button className="bg-[#cdaf8c] ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] rounded-lg h-11 font-semibold" onClick={() => { console.log("Here") }}>
               <p className="w-60">Explore Story SZN Collection</p>
             </button>
@@ -27,7 +27,7 @@ const App = () => {
         </div>
         <div>
           <img src="/cover1152x540.webp" />
-          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 w-11/12 p-2">
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 xl:-translate-y-[400%] 2xl:-translate-y-[800%] w-11/12 p-2">
             <button className="bg-[#cdaf8c] ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] rounded-lg h-11 font-semibold" onClick={() => { console.log("Here") }}>
               <p className="w-[200px]">Explore Fall Collection</p>
             </button>
