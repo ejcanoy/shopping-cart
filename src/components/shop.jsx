d52a3deee9b8d3dc7ba0402caaf384caa4169e52
ejcanoy <euan.james.canoy@gmail.com>
2023-10-26 10:33:34 -0700

    deleted unneeded code

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,17 +1,11 @@
-import { Link } from "react-router-dom"
 import Footer from "./footer"
 import Navbar from "./navbar"
 
 function Shop() {
-    const productName = "product1";
+
     return (
         <>
             <Navbar />
-            <Link to={`/product-page/${productName}`}>
-            <button>
-                Product 1
-            </button>
-            </Link>
             <Footer />
         </>
     )
