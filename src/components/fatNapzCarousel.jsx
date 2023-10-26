0720b6657c3888b9a2a9b68226cf6d0345e81adf
ejcanoy <euan.james.canoy@gmail.com>
2023-10-26 10:28:29 -0700

    fixed carousel buttons

--- src/components/fatNapzCarousel.jsx
+++ src/components/fatNapzCarousel.jsx
@@ -9,24 +9,24 @@ function FatNapzCarousel() {
                 <div>
                     <img src="/cover1152x540.webp" />
                     <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 xl:-translate-y-[400%] 2xl:-translate-y-[800%]  w-11/12 p-2">
-                        <button className="bg-white ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] h-11 font-semibold" onClick={() => { console.log("Here") }}>
-                            <p className="w-[200px]">Explore Fall Collection</p>
+                        <button className="bg-white ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] h-11 font-semibold px-2" onClick={() => { console.log("Here") }}>
+                            <span className="w-[200px]">Explore Fall Collection</span>
                         </button>
                     </div>
                 </div>
                 <div>
                     <img src="/storySZN960x450.webp" />
                     <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 xl:-translate-y-[400%] 2xl:-translate-y-[800%] w-11/12 p-2">
-                        <button className="bg-[#cdaf8c] ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] rounded-lg h-11 font-semibold" onClick={() => { console.log("Here") }}>
-                            <p className="w-60">Explore Story SZN Collection</p>
+                        <button className="bg-white ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] h-11 font-semibold px-2" onClick={() => { console.log("Here") }}>
+                        <span className="w-60">Explore Story SZN Collection</span>
                         </button>
                     </div>
                 </div>
                 <div>
                     <img src="/cover1152x540.webp" />
                     <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 xl:-translate-y-[400%] 2xl:-translate-y-[800%] w-11/12 p-2">
-                        <button className="bg-[#cdaf8c] ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] rounded-lg h-11 font-semibold" onClick={() => { console.log("Here") }}>
-                            <p className="w-[200px]">Explore Fall Collection</p>
+                        <button className="bg-white ease-in-out delay-150 duration-150 hover:bg-black hover:text-[#cdaf8c] h-11 font-semibold px-2" onClick={() => { console.log("Here") }}>
+                        <span className="w-[200px]">Explore Fall Collection</span>
                         </button>
                     </div>
                 </div>
